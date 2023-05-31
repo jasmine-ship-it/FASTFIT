50b8565a0a0001d50b6fb7e6c7477c9a58400b50
Jasmine Hui <jasminehui293@gmail.com>
2023-05-31 14:19:09 +0100

    update to styling and intro text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { AthleteContext } from "../contexts/athlete.contexts";
 import Box from "@mui/material/Box";
+import { HomeContainer } from "./home.styles";
 
 const Home = () => {
   const { athleteData } = useContext(AthleteContext);
@@ -11,20 +12,23 @@ const Home = () => {
         style={{
           backgroundImage: `url(https://images.unsplash.com/photo-1553969546-6f7388a7e07c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=776&q=80)`,
           backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
           height: "100vh",
+          width: "100vw",
         }}
       />
-      <h1>
-        Welcome {athleteData.firstname}
-        {athleteData.lastname}!
-      </h1>
 
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi
-        exercitationem laudantium omnis unde reprehenderit dolorem
-        necessitatibus, et expedita assumenda distinctio neque architecto,
-        dolores eveniet hic sit provident eaque optio. Ullam?
-      </p>
+      <HomeContainer>
+        <h1>
+          Welcome {athleteData.firstname}
+          {athleteData.lastname}!
+        </h1>
+        <p>
+          "Welcome to our running app! Get ready to track your progress and
+          connect with fellow runners. Click on settings to connect to Strava
+          API and start your journey!"
+        </p>
+      </HomeContainer>
     </div>
   );
 };
