6cb299081e893a0702fdca81e96fa62ea1b75b39
Jasmine Hui <jasminehui293@gmail.com>
2023-06-01 16:08:35 +0100

    fixed text location, updated background style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
 import { AthleteContext } from "../contexts/athlete.contexts";
 import Box from "@mui/material/Box";
-import { HomeContainer } from "./home.styles";
+import { HomeContainer, TextContainer } from "./home.styles";
 
 const Home = () => {
   const { athleteData } = useContext(AthleteContext);
 
   return (
-    <div>
+    <HomeContainer>
       <Box
         style={{
           backgroundImage: `url(https://images.unsplash.com/photo-1553969546-6f7388a7e07c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=776&q=80)`,
@@ -15,11 +15,9 @@ const Home = () => {
           backgroundRepeat: "no-repeat",
           height: "100vh",
           backgroundPosition: "center",
-          // width: "100vw",
         }}
       />
-
-      <HomeContainer>
+      <TextContainer>
         <h1>
           Welcome {athleteData.firstname}
           {athleteData.lastname}!
@@ -29,8 +27,8 @@ const Home = () => {
           connect with fellow runners. Click on settings to connect to Strava
           API and start your journey!"
         </p>
-      </HomeContainer>
-    </div>
+      </TextContainer>
+    </HomeContainer>
   );
 };
 
--- src/pages/home.styles.jsx
+++ src/pages/home.styles.jsx
@@ -1,14 +1,24 @@
 import styled from "styled-components";
 
-export const HomeContainer = styled.div`
+export const TextContainer = styled.div`
   background-color: rgba(255, 255, 255, 0.2);
   position: fixed;
-  bottom: 0;
+  top: 0;
   left: 0;
   width: 50%;
   margin-left: 25%;
   height: auto;
-  margin-bottom: 50vh;
+  margin-top: 30vh;
   padding: 10px;
   aligncontent: center;
+`;
+
+export const HomeContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: auto;
+  aligncontent: center;
+  z-index: -1;
 `;
